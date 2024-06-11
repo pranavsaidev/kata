3c0ee119e0a77b24d995d9753594c04ef227c213
Pranav Sai <pranavredrouthu@gmail.com>
2024-06-11 23:13:40 +0530

    Add queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,47 @@
+type Node<T> = {
+    value: T,
+    next?: Node<T>,
+};
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head: Node<T> | undefined;
+    private tail: Node<T> | undefined;
 
     constructor() {
+        this.length = 0;
+        this.tail = this.head = undefined;
     }
 
     enqueue(item: T): void {
+        this.length++;
+        const node: Node<T> = { value: item }
 
-}
+        if (!this.tail) {
+            this.tail = this.head = node;
+            return;
+        }
+
+        this.tail.next = node;
+        this.tail = node;
+    }
     deque(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        const h = this.head;
+        this.head = this.head.next;
+        h.next = undefined;
+
+        this.length--;
+        if (this.length === 0) {
+            this.tail = undefined;
+        }
+
+        return h.value;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
