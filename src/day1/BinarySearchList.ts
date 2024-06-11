18c0609d73dcce8ab8f7b6c4a9c6bb7e21cea26b
Pranav Sai <pranavredrouthu@gmail.com>
2024-06-11 23:00:34 +0530

    Add search & sort

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,18 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0, hi = haystack.length;
 
+    do {
+        const mid = Math.floor(lo + (hi - lo) / 2);
+        const midValue = haystack[mid];
+
+        if (midValue === needle) {
+            return true;
+        } else if (midValue < needle) {
+            lo = mid + 1;
+        } else {
+            hi = mid;
+        }
+    } while (lo < hi);
+
+    return false;
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,19 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const interval = Math.floor(Math.sqrt(breaks.length));
 
+    let curr = interval;
+    for (; curr < breaks.length; curr++) {
+        if (breaks[curr]) {
+            break;
+        }
+    }
+
+    curr -= interval;
+    for (; curr < breaks.length; curr++) {
+        if (breaks[curr]) {
+            return curr;
+        }
+    }
+
+    return -1;
 }
